8828f654ba886aa2dc5782650748662282fecbce
Hubert Lefevre <hubelefevre@gmail.com>
2014-06-25 17:24:06 +0200

    My tracks Option Dialog
        With fucking DatePicker that does not want to be initialized and
        impossible to get the date selected, so cool!!
        And I'm force to go through getViewById for something as simple as
        putting a text in a button for which there is a tweak for it just
        because using .run stopped the app. Super

--- src/net/paluche/Options.scala
+++ src/net/paluche/Options.scala
@@ -10,6 +10,14 @@ import android.app.ActionBar
 import android.widget.{Switch, TextView, CheckBox, Button, DatePicker}
 import android.os.Bundle
 
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.GregorianCalendar
+import java.util.Calendar
+
+import scala.util.control._
+
 class TrackingOption(var trackingOn: Boolean, var trackingStill: Boolean,
   var trackingWalking: Boolean, var trackingRunning: Boolean,
   var trackingInVehicle: Boolean, var trackingOnBicycle : Boolean)
@@ -22,6 +30,9 @@ object OptionTest {
   var trackingRunning:   Boolean = true
   var trackingInVehicle: Boolean = true
   var trackingOnBicycle: Boolean = true
+
+  var startDate: Long = (new Date(114, 0, 1)).getTime//(new GregorianCalendar(14, 1, 1)).getGregorianChange.getTime
+  var stopDate: Long = (new Date()).getTime()
 }
 
 class TrackingOptionActivity extends Activity
@@ -30,11 +41,11 @@ with IdGeneration {
   import OptionTest._
 
   var trackingOnSwitch:  Switch   = null
-  var stillCheckbox:     CheckBox = null
-  var walkingCheckbox:   CheckBox = null
-  var runningCheckbox:   CheckBox = null
-  var inVehicleCheckbox: CheckBox = null
-  var onBicycleCheckbox: CheckBox = null
+  var stillCheckBox:     CheckBox = null
+  var walkingCheckBox:   CheckBox = null
+  var runningCheckBox:   CheckBox = null
+  var inVehicleCheckBox: CheckBox = null
+  var onBicycleCheckBox: CheckBox = null
 
   lazy val trackingOptionLayout = l[VerticalLinearLayout](
     w[Switch]   <~ id(Id.trackingOnSwitch) <~ text("Tracking On"),
@@ -56,23 +67,23 @@ with IdGeneration {
       ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_CUSTOM |
       ActionBar.DISPLAY_USE_LOGO | ActionBar.DISPLAY_SHOW_TITLE)
 
-    // Get the different switch and checkboxes
-    trackingOnSwitch  = findViewById(Id.trackingOnSwitch).asInstanceOf[Switch]
-    stillCheckbox     = findViewById(Id.stillCheckBox).asInstanceOf[CheckBox]
-    walkingCheckbox   = findViewById(Id.walkingCheckBox).asInstanceOf[CheckBox]
-    runningCheckbox   = findViewById(Id.runningCheckBox).asInstanceOf[CheckBox]
-    inVehicleCheckbox = findViewById(Id.inVehicleCheckBox).asInstanceOf[CheckBox]
-    onBicycleCheckbox = findViewById(Id.onBicycleCheckBox).asInstanceOf[CheckBox]
-
     // Set the display according
     val trackingOption:TrackingOption = getTrackingOption()
 
+    // Get the different switch and checkboxes
+    trackingOnSwitch  = findViewById(Id.trackingOnSwitch).asInstanceOf[Switch]
+    stillCheckBox     = findViewById(Id.stillCheckBox).asInstanceOf[CheckBox]
+    walkingCheckBox   = findViewById(Id.walkingCheckBox).asInstanceOf[CheckBox]
+    runningCheckBox   = findViewById(Id.runningCheckBox).asInstanceOf[CheckBox]
+    inVehicleCheckBox = findViewById(Id.inVehicleCheckBox).asInstanceOf[CheckBox]
+    onBicycleCheckBox = findViewById(Id.onBicycleCheckBox).asInstanceOf[CheckBox]
+
     trackingOnSwitch.setChecked(trackingOption.trackingOn)
-    stillCheckbox.setChecked(trackingOption.trackingStill)
-    walkingCheckbox.setChecked(trackingOption.trackingWalking)
-    runningCheckbox.setChecked(trackingOption.trackingRunning)
-    inVehicleCheckbox.setChecked(trackingOption.trackingInVehicle)
-    onBicycleCheckbox.setChecked(trackingOption.trackingOnBicycle)
+    stillCheckBox.setChecked(trackingOption.trackingStill)
+    walkingCheckBox.setChecked(trackingOption.trackingWalking)
+    runningCheckBox.setChecked(trackingOption.trackingRunning)
+    inVehicleCheckBox.setChecked(trackingOption.trackingInVehicle)
+    onBicycleCheckBox.setChecked(trackingOption.trackingOnBicycle)
   }
 
   override def onStop() {
@@ -82,45 +93,100 @@ with IdGeneration {
 
   // FIXME The end of the class is designed for tests. The final
   // implementation is to be done.
+  // TODO get the preferences
   def getTrackingOption(): TrackingOption = new TrackingOption(
     trackingOn, trackingStill, trackingWalking, trackingRunning,
     trackingInVehicle, trackingOnBicycle)
 
   lazy val saveTrackingOption = {
     //  TrackingOption = new TrackingOption(trackingOnSwitch.isChecked,
-      //    stillCheckbox.isChecked, walkingCheckbox.isChecked,
-      //    runningCheckbox.isChecked, inVehicleCheckbox.isChecked,
-      //    onBicycleCheckbox.isChecked)
-    trackingOn  = trackingOnSwitch.isChecked
-    trackingStill     = stillCheckbox.isChecked
-    trackingWalking   = walkingCheckbox.isChecked
-    trackingRunning   = runningCheckbox.isChecked
-    trackingInVehicle = inVehicleCheckbox.isChecked
-    trackingOnBicycle = onBicycleCheckbox.isChecked
+    //    stillCheckBox.isChecked, walkingCheckBox.isChecked,
+    //    runningCheckBox.isChecked, inVehicleCheckBox.isChecked,
+    //    onBicycleCheckBox.isChecked)
+    // TODO save in preferences
+    trackingOn        = trackingOnSwitch.isChecked
+    trackingStill     = stillCheckBox.isChecked
+    trackingWalking   = walkingCheckBox.isChecked
+    trackingRunning   = runningCheckBox.isChecked
+    trackingInVehicle = inVehicleCheckBox.isChecked
+    trackingOnBicycle = onBicycleCheckBox.isChecked
     toast("Tracking options saved") <~ fry
   }
 }
 
-class MyTracksOptionActivity extends Activity with Contexts[Activity] {
-  val myTracksOptionLayout = l[VerticalLinearLayout](
-    l[HorizontalLinearLayout](
-      w[TextView] <~ text("Start Date"),
-      w[DatePicker]),
-    l[HorizontalLinearLayout](
-      w[TextView] <~ text("Stop Date"),
-      w[DatePicker]))
+class MyTracksOptionActivity extends Activity
+with Contexts[Activity]
+with IdGeneration {
+  import OptionTest._
 
-    override def onCreate(b: Bundle) {
-      super.onCreate(b)
-      setContentView(myTracksOptionLayout.get)
+  var startDateButton: Button = null
+  var stopDateButton: Button  = null
 
-      val actionBar: ActionBar = getActionBar
+  var dateFormat: SimpleDateFormat = null
 
-      actionBar.setDisplayOptions(
-        ActionBar.DISPLAY_HOME_AS_UP,
-        ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_CUSTOM |
-        ActionBar.DISPLAY_USE_LOGO | ActionBar.DISPLAY_SHOW_TITLE)
-    }
+  var dateDialogLayout = w[DatePicker]
+  lazy val datePicker: DatePicker = dateDialogLayout.get.asInstanceOf[DatePicker]
+
+  def startDateButtonClick: Ui[Any] = {
+    var start = Calendar.getInstance()
+    start.setTimeInMillis(startDate)
+
+    dateDialogLayout.get.asInstanceOf[DatePicker].init(start.get(Calendar.YEAR),
+      start.get(Calendar.MONTH), start.get(Calendar.DATE), null)
+
+    dialog(dateDialogLayout) <~ title("Start date") <~
+      positiveYes(Ui(saveMyTracksOption(Id.startDatePickerDialog))) <~ speak
   }
 
+  lazy val stopDatePickerDialog = dialog(w[DatePicker] <~
+    id(Id.stopDatePicker)) <~ title("Stop date") <~
+    positiveYes(Ui(saveMyTracksOption(Id.stopDatePickerDialog))) <~ speak
 
+  lazy val myTracksOptionLayout = l[VerticalLinearLayout](
+      w[TextView] <~ text("Start Date:"),
+      w[Button]   <~ id(Id.startDateButton) <~ On.click(startDateButtonClick),
+      w[TextView] <~ text("\nStop Date:"),
+      w[Button]   <~ id(Id.stopDateButton) <~ On.click(stopDatePickerDialog)
+    )
+
+  override def onCreate(b: Bundle) {
+    super.onCreate(b)
+    setContentView(myTracksOptionLayout.get)
+
+    val actionBar: ActionBar = getActionBar
+    actionBar.setDisplayOptions(
+      ActionBar.DISPLAY_USE_LOGO | ActionBar.DISPLAY_SHOW_TITLE,
+      ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_SHOW_CUSTOM |
+      ActionBar.DISPLAY_USE_LOGO | ActionBar.DISPLAY_SHOW_TITLE)
+
+    startDateButton = findViewById(Id.startDateButton).asInstanceOf[Button]
+    stopDateButton  = findViewById(Id.stopDateButton).asInstanceOf[Button]
+    // Set the date slot to the saved date from preferences.
+    // TODO get the preferences.
+    try {
+      dateFormat = DateFormat.getDateTimeInstance().asInstanceOf[SimpleDateFormat]
+    } catch {
+      case NonFatal(exc) => logE"Internat error: date formatting exeption."
+    }
+
+    dateFormat.applyPattern("yyyy-MM-dd")
+    startDateButton.setText(dateFormat.format(new Date(startDate)))
+    stopDateButton.setText(dateFormat.format(new Date(stopDate)))
+  }
+
+  def saveMyTracksOption(datePickerId: Int) {
+    // TODO retrieve dates preferences
+    // Update date preferences
+    if (datePickerId == Id.startDatePicker) {
+      startDate = (new GregorianCalendar(datePicker.getYear, datePicker.getMonth,
+        datePicker.getDayOfMonth)).getGregorianChange.getTime
+    } else if (datePickerId == Id.stopDatePicker) {
+      stopDate = (new GregorianCalendar(datePicker.getYear, datePicker.getMonth,
+        datePicker.getDayOfMonth)).getGregorianChange.getTime
+    }
+
+    // Update display
+    startDateButton.setText(dateFormat.format(new Date(startDate)))
+    stopDateButton.setText(dateFormat.format(new Date(stopDate)))
+  }
+}
